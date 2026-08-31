14620bd70cf29dd66470268a16b0425eb995bb43
vk4011 <gvikramram63@gmail.com>
2026-08-31 22:17:20 +0530

    fix ✅

--- src/main/java/app/BackendApplication.java
+++ src/main/java/app/BackendApplication.java
@@ -3,11 +3,18 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(e
+                -> System.setProperty(e.getKey(), e.getValue())
+        );
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
